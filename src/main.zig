268fcbb07ae16ec88456110eb646fd3202f491c3
Josh Lyons <jlolslh@gmail.com>
2025-05-20 19:38:26 -0500

    Can consume a buffer of input from SDL

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const c = @cImport({
     @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cDefine("SDL_MAIN_HANDLED", {});
     @cInclude("SDL3/SDL.h");
     @cInclude("SDL3/SDL_revision.h");
     @cInclude("SDL3/SDL_main.h");
@@ -14,7 +15,8 @@ var renderer: *c.SDL_Renderer = undefined;
 pub fn main() !void {
     // Variables
     var event: c.SDL_Event = undefined;
-    var input: []const u8 = "";
+    var input_buffer: [256]u8 = undefined; // buffer for input
+    var input_len: usize = 0;
 
     // Init the video driver
     _ = c.SDL_Init(c.SDL_INIT_VIDEO);
@@ -27,13 +29,25 @@ pub fn main() !void {
         if (event.type == c.SDL_EVENT_QUIT) {
             break;
         } else if (event.type == c.SDL_EVENT_KEY_DOWN) {
-            input = std.mem.span(c.SDL_GetKeyName(c.SDL_GetKeyFromScancode(event.key.scancode, c.SDL_KMOD_NONE, true)));
+            const keycode = c.SDL_GetKeyFromScancode(event.key.scancode, c.SDL_KMOD_NONE, true);
+            if (keycode == c.SDLK_RETURN or keycode == c.SDLK_KP_ENTER) {
+                // Enter pressed: process input (here, just clear it)
+                input_len = 0;
+            } else if (keycode == c.SDLK_BACKSPACE) {
+                if (input_len > 0) input_len -= 1;
+            } else {
+                // Only handle printable ASCII
+                if (keycode >= 32 and keycode < 127 and input_len < input_buffer.len - 1) {
+                    input_buffer[input_len] = @intCast(keycode);
+                    input_len += 1;
+                }
+            }
         }
         _ = c.SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
         _ = c.SDL_RenderClear(renderer);
 
         _ = c.SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
-        _ = c.SDL_RenderDebugText(renderer, 5, 5, input.ptr);
+        _ = c.SDL_RenderDebugText(renderer, 5, 5, &input_buffer);
 
         _ = c.SDL_RenderPresent(renderer);
     }
